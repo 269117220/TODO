1125552ead7c4f1aea06c406967626c49d41f36d
269117220 <269117220@qq.com>
2018-03-19 09:41:08 +0800

    es6 ->es5

--- public/js/pro/tool.js
+++ public/js/pro/tool.js
@@ -11,4 +11,19 @@ function getCurrTime(){
     var currDate=year+'-'+month+'-'+day+' '+hours+':'+minus;
     return currDate;
 }
+/*Polyfill:includes*/
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+    
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
 
